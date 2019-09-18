b8d3e9b0bb94d4072c39c95fffcdfa63a09f2e2c
Matt Bone <thatmattbone@gmail.com>
2019-09-17 21:40:11 -0500

    hooray I can read files

--- advent/2018/Advent2018/Day1/Program.cs
+++ advent/2018/Advent2018/Day1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Day1
 {
     class Program
     {
+        private static string INPUT_PATH = "/home/mbone/Developer/lab/advent/2018/Advent2018/Day1/input";
+        
+        static int stringToInt(string inputString)
+        {
+            return Int32.Parse(inputString);
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 99;
+            var j = -50;
+            
+            using (FileStream fs = File.OpenRead(INPUT_PATH))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b,0,b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }                
+            }
         }
     }
 }
