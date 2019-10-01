c597307806aebf2aeb913c54047e235448590db7
Matt Bone <thatmattbone@gmail.com>
2019-10-01 08:00:26 -0500

    read file for day 2 input

--- advent/2018/Advent2018/Day2/ProgramDay2.cs
+++ advent/2018/Advent2018/Day2/ProgramDay2.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Day2
 {
     class ProgramDay2
     {
+        private static string INPUT_PATH = "/home/mbone/Developer/lab/advent/2018/Advent2018/Day2/input";
+        
+        static IEnumerable<string> fileToStringStream()
+        {
+            using (StreamReader sr = new StreamReader(INPUT_PATH))
+            {
+                string myStr;
+                while ((myStr = sr.ReadLine()) != null)
+                {
+                    yield return myStr;
+                }
+            }
+        }
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var i in fileToStringStream())
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
