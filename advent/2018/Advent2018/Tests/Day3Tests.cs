a58aacf8544de7d80eda5c5cab618ac11e6f7223
Matt Bone <thatmattbone@gmail.com>
2019-10-19 12:46:26 -0500

    add stub tests for day3

---
+++ advent/2018/Advent2018/Tests/Day3Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Day2;
+
+namespace Tests
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void StubTest()
+        {
+            Assert.Equal(1, 3);
+        }
+        
+    }
+}
