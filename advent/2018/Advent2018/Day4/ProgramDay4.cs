9058ac47dbbbd46026b5b3470fc57b4f61d7cbf7
Matt Bone <thatmattbone@gmail.com>
2019-10-21 22:54:54 -0500

    day 4 guard log parsing and sorting stubbed out

--- advent/2018/Advent2018/Day4/ProgramDay4.cs
+++ advent/2018/Advent2018/Day4/ProgramDay4.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using System.IO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Day4
 {
+    public struct GuardLog
+    {
+        public DateTime timestamp;
+        public string action;
+        public string orginalLine;
+    }
     public class ProgramDay4
     {
+        private static string INPUT_PATH = "/home/mbone/Developer/lab/advent/2018/Advent2018/Day4/input";
+
+        static IEnumerable<string> fileToStringStream(string input_path)
+        {
+            using (StreamReader sr = new StreamReader(input_path))
+            {
+                string myStr;
+                while ((myStr = sr.ReadLine()) != null)
+                {
+                    yield return myStr;
+                }
+            }
+        }
+
+        static List<GuardLog> sortInputFile()
+        {
+            List<GuardLog> guardLogs = new List<GuardLog>();
+            
+            foreach (string s in fileToStringStream(INPUT_PATH))
+            {
+                string datePart = s.Substring(1, 16);
+                string rest = s.Substring(19);
+
+                var guardLog = new GuardLog();
+                guardLog.timestamp = DateTime.Parse(datePart);
+                guardLog.action = rest;
+                guardLog.orginalLine = s;
+
+                guardLogs.Add(guardLog);
+            }
+
+            guardLogs.Sort((guard1, guard2) => guard1.timestamp.CompareTo(guard2.timestamp));
+            
+            return guardLogs;
+        }
+
+        static void writeGuardLogs(List<GuardLog> guardLogs)
+        {
+            
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sortInputFile();
         }
     }
 }
