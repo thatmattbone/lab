be7d519506d6a20c9afeb0b7074d3c53cc827901
Matt Bone <thatmattbone@gmail.com>
2019-10-21 20:10:25 -0500

    stub out fabric generation for day3, part 1

---
+++ advent/2018/Advent2018/Day3/ProgramDay3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day3
+{
+    class ProgramDay3
+    {
+
+        public static List<int>[,] BuildInitialFabric(int fabricSize)
+        {
+            List<int>[,] fabric = new List<int>[fabricSize, fabricSize];
+
+            for (int i = 0; i < fabricSize; i++)
+            {
+                for (int j = 0; j < fabricSize; j++)
+                {
+                    fabric[i, j] = new List<int>();
+                }
+            }
+            return fabric;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            var fabric = BuildInitialFabric(1000);
+            
+            fabric[10, 10].Add(0);
+            fabric[10, 10].Add(1);
+            
+            Console.WriteLine(fabric[1, 2].Count);
+            Console.WriteLine(fabric[10, 10].Count);
+        }
+    }
+}
