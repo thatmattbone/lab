ac7a937bbd485dc4fcf08217ecf25a2b7830f2fc
Matt Bone <thatmattbone@gmail.com>
2019-11-04 19:31:09 -0600

    checkpoint some more linq crap

--- advent/2018/Advent2018/Tests/LinqFunTests.cs
+++ advent/2018/Advent2018/Tests/LinqFunTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Utils;
@@ -6,6 +7,12 @@ using Xunit;
 
 namespace Tests
 {
+    struct TestStruct
+    {
+        public int foo;
+        public int bar;
+    }
+
     public class LinqFunTests
     {
         [Fact]
@@ -33,17 +40,62 @@ namespace Tests
         [Fact]
         public void TestLinqSelections()
         {
-            int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
             Assert.Equal(
                 new List<int> {1},
-                myArray.Where(x => x == 1).ToList());
+                myIntArray.Where(x => x == 1).ToList());
             
-            var foo = from myInt in myArray where myInt == 1 select myInt;
+            var foo = from myInt in myIntArray where myInt == 1 select myInt;
             
             Assert.Equal(
                 new List<int> {1},
                 foo.ToList()
+            );
+            
+            TestStruct[] myObjectArray =
+            {
+                new TestStruct {foo = 1, bar = 2},
+                new TestStruct {foo = 1, bar = 3},
+                new TestStruct {foo = 1, bar = 4}
+            };
+            
+            Assert.Equal(
+                new List<TestStruct> {
+                    new TestStruct {foo = 1, bar = 2},
+                    new TestStruct {foo = 1, bar = 4}},
+                myObjectArray.Where(x => x.bar % 2 == 0).ToList()
+            );
+        }
+
+        [Fact]
+        public void TestLinqGroupBy()
+        {
+            TestStruct[] myObjectArray =
+            {
+                new TestStruct {foo = 1, bar = 2},
+                new TestStruct {foo = 1, bar = 3},
+                new TestStruct {foo = 2, bar = 4}
+            };
+            
+            Dictionary<int, List<TestStruct>> myStructHisto = new Dictionary<int, List<TestStruct>>();
+            myStructHisto[1] = new List<TestStruct>
+            {
+                new TestStruct {foo = 1, bar = 2},
+                new TestStruct {foo = 1, bar = 3},
+            };
+            
+            myStructHisto[2] = new List<TestStruct>
+            {
+                new TestStruct {foo = 2, bar = 4}
+            };
+
+            var foo = myObjectArray
+                .GroupBy(x => x.foo)
+                .ToDictionary(x => x.Key, x => x.ToList());
+            Assert.Equal(
+                myStructHisto,
+                foo
             );
         }
     }
