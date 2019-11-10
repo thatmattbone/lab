b1eee8768f43093781ed9302650421af8963863f
Matt Bone <thatmattbone@gmail.com>
2019-11-10 09:36:32 -0600

    adding day 5 stub and tests

---
+++ advent/2018/Advent2018/Day5/ProgramDay5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day5
+{
+    public class ProgramDay5
+    {
+        public static int answerPart1()
+        {
+            return -1;
+        }
+
+        public static int answerPart2()
+        {
+            return -2;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(answerPart1());
+            Console.WriteLine(answerPart2());
+        }
+    }
+}
---
+++ advent/2018/Advent2018/Tests/Day5Tests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using Day5;
+
+namespace Tests
+{
+
+    public class Day5Tests
+    {
+        [Fact]
+        public void TestDay5Part1()
+        {
+            Assert.Equal(ProgramDay5.answerPart1(), -1);
+        }
+
+        [Fact]
+        public void TestDay5Part2()
+        {
+            Assert.Equal(ProgramDay5.answerPart2(), -2);
+        }
+    }
+}
