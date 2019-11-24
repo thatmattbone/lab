e299585656ad927bb6926d0508d66ec6053213f8
Matt Bone <thatmattbone@gmail.com>
2019-11-24 12:12:43 -0600

    read day 6 input file

--- advent/2018/Advent2018/Day6/ProgramDay6.cs
+++ advent/2018/Advent2018/Day6/ProgramDay6.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using Utils;
 
 namespace Day6
 {
     public class ProgramDay6
     {
+        public static string INPUT_PATH = "/home/mbone/Developer/lab/advent/2018/Advent2018/Day6/input";
+
+        public static List<(int, int)> getCoords()
+        {
+            var coords = new List<(int, int)>();
+
+            foreach (string input in Streams.fileToStringStream(INPUT_PATH))
+            {
+                var splitInput = input.Split(",");
+                coords.Add((Int32.Parse(splitInput[0]), Int32.Parse(splitInput[1])));
+            }
+            
+            return coords;
+        }
+        
         public static int answerPart1()
         {
+            foreach (var coord in getCoords())
+            {
+                Console.WriteLine(coord);
+            }
             return -1;
         }
 
