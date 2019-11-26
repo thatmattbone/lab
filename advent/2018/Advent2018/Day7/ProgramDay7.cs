d065637aa5408f29ac032605e8175a8b41dac3a0
Matt Bone <thatmattbone@gmail.com>
2019-11-26 08:51:26 -0600

    stub out day 7

---
+++ advent/2018/Advent2018/Day7/ProgramDay7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Utils;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Day7
+{
+
+    
+    public class ProgramDay7
+    {
+        public static string INPUT_PATH = "/home/mbone/Developer/lab/advent/2018/Advent2018/Dayy/input";
+
+        
+        public static int answerPart1()
+        {
+            return -1;
+        }
+
+        public static int answerPart2()
+        {
+            return -2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(answerPart1());
+            Console.WriteLine(answerPart2());
+        }
+    }
+}
---
+++ advent/2018/Advent2018/Tests/Day7Tests.cs
@@ -0,0 +1,21 @@
+using Day7;
+using Xunit;
+
+
+namespace Tests
+{
+    public class Day7Tests
+    {
+        [Fact]
+        public void TestDay7Part1()
+        {
+            Assert.Equal(ProgramDay7.answerPart1(), -1);
+        }
+
+        [Fact]
+        public void TestDay7Part2()
+        {
+            Assert.Equal(ProgramDay7.answerPart2(), -2);
+        }        
+    }
+}
