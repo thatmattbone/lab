b9f5302979714f8a681fbad33ea603acec106f2d
Matt Bone <thatmattbone@gmail.com>
2019-12-16 17:17:26 -0600

    broken day 8 code

--- advent/2018/Advent2018/Day8/ProgramDay8.cs
+++ advent/2018/Advent2018/Day8/ProgramDay8.cs
@@ -10,9 +10,39 @@ namespace Day8
     {
         public static string INPUT_PATH = "/home/mbone/Developer/lab/advent/2018/Advent2018/Day8/input";
 
+        public static int sumMetadata(int[] tree)
+        {
+            if (tree.Length == 0)
+            {
+                return 0;
+            }
+            string treeString = "";
+            foreach (var i in tree)
+            {
+                treeString += " " + i;
+            }
+            Console.WriteLine("tree = " + treeString);            
+
+            int numChildren = tree[0];
+            int numMetadata = tree[1];
+            //Console.WriteLine("num metadata: " + numMetadata);
+            
+            int sumOfOurMetadata = tree[^numMetadata..].Sum();
+            //Console.WriteLine("sum metadata: " + sumOfOurMetadata);
+
+            string recurSlice = "";
+            foreach (var i in tree[2..^numMetadata])
+            {
+                recurSlice += " " + i;
+            }
+            //Console.WriteLine("sum metadata slice = " + recurSlice);
+
+            return sumOfOurMetadata + sumMetadata(tree[2..^numMetadata]);
+        }
+        
         public static int answerPart1()
         {
-            return -1;
+            return sumMetadata(new int[] {2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2}, 1);
         }
 
         public static int answerPart2()
