3c75ef27d50acfcdffff7490d1a2fd5726287ede
Matt Bone <thatmattbone@gmail.com>
2021-04-19 21:44:22 -0500

    restart the accept() when it crashes

--- elixir_fun/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ elixir_fun/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -11,7 +11,7 @@ defmodule KVServer.Application do
 
     children = [
       {Task.Supervisor, name: KVServer.TaskSupervisor},
-      {Task, fn -> KVServer.accept(port) end}
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
