9d61bb9142db9d610d641a3d76beecf52177971e
Matt Bone <thatmattbone@gmail.com>
2021-12-03 17:58:53 -0600

    parse day 2 input

--- advent/2021/advent/lib/day_02.ex
+++ advent/2021/advent/lib/day_02.ex
@@ -1,6 +1,14 @@
 defmodule Day02 do
 
   def part1() do
+    body = File.read!("input/input_02")
+    split_body = String.split(body, "\n")
+    instructions_and_magnitudes = for i <- split_body, String.length(i) > 0 do
+      [instruction, magnitude] = String.split(i, " ")
+      {instruction, String.to_integer(magnitude)}
+    end
+
+    IO.inspect(instructions_and_magnitudes)
     1
   end
 
