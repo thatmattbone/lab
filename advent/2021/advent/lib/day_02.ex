6f72ca9338ce8b86ef03e775ab47b14ef5e6a40e
Matt Bone <thatmattbone@gmail.com>
2021-12-03 18:22:20 -0600

    day 2, part 2 works

--- advent/2021/advent/lib/day_02.ex
+++ advent/2021/advent/lib/day_02.ex
@@ -32,7 +32,34 @@ defmodule Day02 do
     depth * magnitude
   end
 
-  def part2() do
-    2
+  def calc_new_position_with_aim({instruction, magnitude}, {depth, position, aim}) when instruction == "forward" do
+    {depth + (aim * magnitude), position + magnitude, aim}
   end
+
+  def calc_new_position_with_aim({instruction, magnitude}, {depth, position, aim}) when instruction == "down" do
+    {depth, position, aim + magnitude}
+  end
+
+  def calc_new_position_with_aim({instruction, magnitude}, {depth, position, aim}) when instruction == "up" do
+    {depth, position, aim - magnitude}
+  end
+
+  def find_position_with_aim([], current_position) do
+    current_position
+  end
+
+  def find_position_with_aim([head|tail], current_position) do
+    find_position_with_aim(tail, calc_new_position_with_aim(head, current_position))
+  end
+
+  def part2() do
+    body = File.read!("input/input_02")
+    split_body = String.split(body, "\n")
+    instructions_and_magnitudes = for i <- split_body, String.length(i) > 0 do
+      [instruction, magnitude] = String.split(i, " ")
+      {instruction, String.to_integer(magnitude)}
+    end
+
+    {depth, magnitude, _} = find_position_with_aim(instructions_and_magnitudes, {0, 0, 0})
+    depth * magnitude  end
 end
