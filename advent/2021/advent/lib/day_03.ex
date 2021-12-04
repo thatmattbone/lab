14e6bf22e8ff2304fb71b1c66375bd6fa65ae33f
Matt Bone <thatmattbone@gmail.com>
2021-12-03 21:42:06 -0600

    checkpoint some day 3 stuff

--- advent/2021/advent/lib/day_03.ex
+++ advent/2021/advent/lib/day_03.ex
@@ -1,6 +1,14 @@
 defmodule Day03 do
 
   def part1() do
+    body = File.read!("input/input_03")
+    split_body = for i <- String.split(body, "\n"), String.length(i) > 0, do: i
+
+    # Enum.with_index(entry)
+    foo = for entry <- split_body, {bit, i} <- Enum.with_index(Kernel.to_charlist(entry)) do
+      {i, Kernel.to_string(bit)}
+    end
+    IO.inspect(foo)
     1
   end
 
