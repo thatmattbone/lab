b668deadc876c9e8d8c2c04706480d2d44965f7d
Matt Bone <thatmattbone@gmail.com>
2021-12-03 18:11:04 -0600

    parse day 2 input

--- advent/2021/advent/lib/day_02.ex
+++ advent/2021/advent/lib/day_02.ex
@@ -1,5 +1,25 @@
 defmodule Day02 do
 
+  def calc_new_position({instruction, magnitude}, {depth, position}) when instruction == "forward" do
+    {depth, position + magnitude}
+  end
+
+  def calc_new_position({instruction, magnitude}, {depth, position}) when instruction == "down" do
+    {depth + magnitude, position}
+  end
+
+  def calc_new_position({instruction, magnitude}, {depth, position}) when instruction == "up" do
+    {depth - magnitude, position}
+  end
+
+  def find_position([], current_position) do
+    current_position
+  end
+
+  def find_position([head|tail], current_position) do
+    find_position(tail, calc_new_position(head, current_position))
+  end
+
   def part1() do
     body = File.read!("input/input_02")
     split_body = String.split(body, "\n")
@@ -8,8 +28,7 @@ defmodule Day02 do
       {instruction, String.to_integer(magnitude)}
     end
 
-    IO.inspect(instructions_and_magnitudes)
-    1
+    find_position(instructions_and_magnitudes, {0, 0})
   end
 
   def part2() do
