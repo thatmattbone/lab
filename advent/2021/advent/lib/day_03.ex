621e7ff495301dcbc6967b1c292a28e9965f3093
Matt Bone <thatmattbone@gmail.com>
2021-12-04 10:54:28 -0600

    add somde day 3 tests

--- advent/2021/advent/lib/day_03.ex
+++ advent/2021/advent/lib/day_03.ex
@@ -1,14 +1,47 @@
 defmodule Day03 do
 
+  def get_initial_map() do
+    %{
+      0 => %{0 => 0, 1 => 0},
+      1 => %{0 => 0, 1 => 0},
+      2 => %{0 => 0, 1 => 0},
+      3 => %{0 => 0, 1 => 0},
+      4 => %{0 => 0, 1 => 0},
+      5 => %{0 => 0, 1 => 0},
+      6 => %{0 => 0, 1 => 0},
+      7 => %{0 => 0, 1 => 0},
+      8 => %{0 => 0, 1 => 0},
+      9 => %{0 => 0, 1 => 0},
+      10 => %{0 => 0, 1 => 0},
+      11 => %{0 => 0, 1 => 0},
+    }
+  end
+
+  def int_to_bin(val) when val == 48, do: 0
+  def int_to_bin(val) when val == 49, do: 1
+
+  def input_str_to_list(input_str) do
+    for {bit, i} <- Enum.with_index(to_charlist(input_str)) do
+      {i, int_to_bin(bit)}
+    end
+  end
+
   def part1() do
     body = File.read!("input/input_03")
     split_body = for i <- String.split(body, "\n"), String.length(i) > 0, do: i
 
+    IO.inspect(hd(split_body))
+    IO.inspect(input_str_to_list(hd(split_body)))
+
     # Enum.with_index(entry)
-    foo = for entry <- split_body, {bit, i} <- Enum.with_index(Kernel.to_charlist(entry)) do
-      {i, Kernel.to_string(bit)}
-    end
-    IO.inspect(foo)
+    # foo = for entry <- split_body,
+    #           {bit, i} <- Enum.with_index(Kernel.to_charlist(entry)), reduce %{} do
+    #   acc -> map.update(acc, i, 1, {i, Kernel.to_string(bit)}
+    # end
+
+    # # require IEx; IEx.pry
+
+    # IO.inspect(foo)
     1
   end
 
--- advent/2021/advent/test/day_03_test.exs
+++ advent/2021/advent/test/day_03_test.exs
@@ -2,6 +2,32 @@ defmodule Day03Test do
   use ExUnit.Case
   doctest Day03
 
+  test "get_initial_map()" do
+    assert map_size(Day03.get_initial_map()) == 12
+  end
+
+  test "int_to_bin()" do
+    assert Day03.int_to_bin(hd(to_charlist("0"))) == 0
+    assert Day03.int_to_bin(hd(to_charlist("1"))) == 1
+  end
+
+  test "input_str_to_list" do
+    assert Day03.input_str_to_list("000110000001") == [
+      {0, 0},
+      {1, 0},
+      {2, 0},
+      {3, 1},
+      {4, 1},
+      {5, 0},
+      {6, 0},
+      {7, 0},
+      {8, 0},
+      {9, 0},
+      {10, 0},
+      {11, 1}
+    ]
+  end
+
   test "Day 03, part 1" do
     assert Day03.part1() == 1
   end
