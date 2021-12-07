7b46097d5d947c2f5dc4cd2626c86c9def228e71
Matt Bone <thatmattbone@gmail.com>
2021-12-06 22:11:45 -0600

    some more work on day 4

--- advent/2021/advent/lib/day_04.ex
+++ advent/2021/advent/lib/day_04.ex
@@ -1,5 +1,27 @@
 defmodule Day04 do
+
+
   def part1() do
+    calls = File.read!("input/input_04_calls")
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    list_of_grids = File.read!("input/input_04_tables")
+      |> String.split("\n", trim: true)
+      |> Enum.chunk_every(5)
+      |> Enum.map(fn table_rows ->
+          Enum.map(table_rows, fn table_row ->
+            table_row |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+          end)
+      end)
+
+    boards = for grid <- list_of_grids do
+      Day04Board.new(grid)
+    end
+
+    [first_board | _] = boards
+    IO.inspect(first_board)
+
     1
   end
 
---
+++ advent/2021/advent/lib/day_04_board.ex
@@ -0,0 +1,45 @@
+defmodule Day04Board do
+  # stolen from jose valim's twitch stream
+  # I followed along to learn some elixir
+  empty_grid = Tuple.duplicate(Tuple.duplicate(false, 5), 5)
+
+  @enforce_keys [:numbers]
+  defstruct [:numbers, grid: empty_grid]
+
+  def new(grid) do
+    for {line, row} <- Enum.with_index(grid),
+        {value, column} <- Enum.with_index(line),
+        into: %{} do
+          {value, {row, column}}
+        end
+  end
+
+  defp is_col_winner?(grid) do
+    Enum.any?(0..4, fn col ->
+      Enum.all?(0..4, fn row ->
+        grid |> elem(row) |> elem(col)
+      end)
+    end)
+  end
+
+  def is_row_winner?(grid) do
+    Enum.any?(0..4, fn row ->
+      elem(grid, row) == {true, true, true, true, true}
+    end)
+  end
+
+  def is_winner?(%Day04Board{grid: grid}) do
+    is_row_winner?(grid) or is_col_winner?(grid)
+  end
+
+  def mark_space(%Day04Board{numbers: numbers} = board, number) do
+    case numbers do
+      %{^number => {row, column}} ->
+        put_in(board, [Access.key(:grid), Access.elem(row), Access.elem(column)], number)
+
+      %{} ->
+        board
+    end
+  end
+
+end
