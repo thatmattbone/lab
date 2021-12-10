b62f827865354a90ecdb3590a9c19afdc7b65271
Matt Bone <thatmattbone@gmail.com>
2021-12-09 22:20:13 -0600

    day 5 parsing code

--- advent/2021/advent/lib/day_05.ex
+++ advent/2021/advent/lib/day_05.ex
@@ -1,5 +1,17 @@
 defmodule Day05 do
   def part1() do
+    lines = File.read!("input/input_05")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> String.split(line, " -> ") end)
+      |> Enum.map(fn [first, second] -> {String.split(first, ","), String.split(second, ",")} end)
+      |> Enum.map(fn {[x1_str, y1_str], [x2_str, y2_str]} ->
+         {
+           {String.to_integer(x1_str), String.to_integer(y1_str)},
+           {String.to_integer(x2_str), String.to_integer(y2_str)}
+         }
+        end)
+    IO.inspect(lines)
+
     1
   end
 
