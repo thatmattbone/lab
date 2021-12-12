1c18c5f1e3428a027f4b5b518b667b1a06f4e760
Matt Bone <thatmattbone@gmail.com>
2021-12-12 09:31:05 -0600

    day 6, part 1 (naive, brute force) works. too slow to do part 2

--- advent/2021/advent/lib/day_06.ex
+++ advent/2021/advent/lib/day_06.ex
@@ -1,11 +1,46 @@
 defmodule Day06 do
 
+  def parse_lines() do
+    File.read!("input/input_06")
+      |> String.split("\n", trim: true)
+      |> hd()
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+  end
+
+  def evolve_one_day(n) when n == 0 do
+    {6, 8}
+  end
+
+  def evolve_one_day(n) do
+    {n - 1, nil}
+  end
+
+  def evolve_list(initial_state, num_days) when num_days == 0 do
+    initial_state
+  end
+
+  def evolve_list(initial_state, num_days) do
+    IO.puts(num_days)
+    IO.puts(length(initial_state))
+
+    evolved = initial_state
+      |> Enum.map(&Day06.evolve_one_day/1)
+
+    evolved_fish = for {i, _j} <- evolved, do: i
+    new_fish = for {_i, j} <- evolved, j != nil, do: j
+
+    evolve_list(evolved_fish ++ new_fish, num_days - 1)  # slow...
+  end
+
   def part1() do
-    1
+    parse_lines()
+      |> evolve_list(80)
+      |> length()
   end
 
   def part2() do
-    2
-  end
-
+    parse_lines()
+      |> evolve_list(256)
+      |> length()  end
 end
--- advent/2021/advent/test/day_06_test.exs
+++ advent/2021/advent/test/day_06_test.exs
@@ -2,6 +2,12 @@ defmodule Day06Test do
   use ExUnit.Case
   doctest Day06
 
+  test "evolve_list" do
+    evolved = Day06.evolve_list([3,4,3,1,2], 18)
+    assert length(evolved) == 26
+    assert evolved == [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
+  end
+
   test "day 06, part 1" do
     assert Day06.part1() == 1
   end
