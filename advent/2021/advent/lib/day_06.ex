ba168032d4c99b436bcabbace1d6f0c5e1d199d9
Matt Bone <thatmattbone@gmail.com>
2021-12-12 10:16:43 -0600

    checkpoint lookup table idea (which also does not work)

--- advent/2021/advent/lib/day_06.ex
+++ advent/2021/advent/lib/day_06.ex
@@ -16,13 +16,12 @@ defmodule Day06 do
     {n - 1, nil}
   end
 
-  def evolve_list(initial_state, num_days) when num_days == 0 do
+  def evolve_list_slow(initial_state, num_days) when num_days == 0 do
     initial_state
   end
 
-  def evolve_list(initial_state, num_days) do
+  def evolve_list_slow(initial_state, num_days) do
     IO.puts(num_days)
-    IO.puts(length(initial_state))
 
     evolved = initial_state
       |> Enum.map(&Day06.evolve_one_day/1)
@@ -30,17 +29,34 @@ defmodule Day06 do
     evolved_fish = for {i, _j} <- evolved, do: i
     new_fish = for {_i, j} <- evolved, j != nil, do: j
 
-    evolve_list(evolved_fish ++ new_fish, num_days - 1)  # slow...
+    evolve_list_slow(evolved_fish ++ new_fish, num_days - 1)  # slow...
+  end
+
+  def sum_list_slow(initial_state, num_days) do
+    length(Day06.evolve_list_slow(initial_state, num_days))
+  end
+
+  def evolve_list_fast(initial_state, num_days) do
+    lookup = %{
+      1 => evolve_list_slow([1], num_days) |> length(),
+      2 => evolve_list_slow([2], num_days) |> length(),
+      3 => evolve_list_slow([3], num_days) |> length(),
+      4 => evolve_list_slow([4], num_days) |> length(),
+      5 => evolve_list_slow([5], num_days) |> length(),
+      6 => evolve_list_slow([6], num_days) |> length(),
+    }
+    IO.puts("built lookup table")
+
+    initial_state
+      |> Enum.map(fn x -> Map.get(lookup, x) end)
+      |> Enum.sum()
   end
 
   def part1() do
-    parse_lines()
-      |> evolve_list(80)
-      |> length()
+    Day06.evolve_list_fast(parse_lines(), 80)
   end
 
   def part2() do
-    parse_lines()
-      |> evolve_list(256)
-      |> length()  end
+    Day06.evolve_list_fast(parse_lines(), 256)
+  end
 end
