88efc945ddd17633b0022642aa9e7dfbb7a37d5c
Matt Bone <thatmattbone@gmail.com>
2021-12-13 18:14:02 -0600

    day 7, parse the input

--- advent/2021/advent/lib/day_07.ex
+++ advent/2021/advent/lib/day_07.ex
@@ -1,6 +1,16 @@
 defmodule Day07 do
 
+  def parse_lines() do
+    File.read!("input/input_07")
+      |> String.split("\n", trim: true)
+      |> hd()
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+  end
+
   def part1() do
+    IO.inspect(Day07.parse_lines())
+
     1
   end
 
