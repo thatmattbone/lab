6903b0b863183f513a7fe677bd00e4ddab5e235f
Matt Bone <thatmattbone@gmail.com>
2021-12-15 20:33:01 -0600

    stub out some stack tests, etc

--- advent/2021/advent/lib/day_10.ex
+++ advent/2021/advent/lib/day_10.ex
@@ -1,9 +1,49 @@
 defmodule Day10 do
+
+  @opening_tags MapSet.new(["(", "[", "{", "<"])
+
+  @closing_tags MapSet.new([")", "]", "}", ">"])
+
+  @tag_map  %{
+    "(" => ")",
+    ")" => "(",
+
+    "[" => "]",
+    "]" => "[",
+
+    "{" => "}",
+    "}" => "{",
+
+    "<" => ">",
+    ">" => "<",
+  }
+
+  def line_info([hd | tail], stack) when length(stack) == 0 do
+    if MapSet.member?(@opening_tags, hd) do
+        line_info(tail, [hd | stack])
+      else
+        {:corrupt, hd}
+    end
+  end
+
+  def line_info([], stack) when length(stack) == 0 do
+    {:complete, 0}
+  end
+
+  def line_info([], stack) do
+    {:incomplete, length(stack)}
+  end
+
+  def line_info(line) do
+    Day10.line_info(line, [])
+  end
+
+
   def part1() do
     File.read!("input/input_10")
       |> String.split("\n", trim: true)
       |> Enum.map(fn line -> String.split(line, "", trim: true) end)
-      |> IO.inspect()
+      #|> Enum.map(fn line -> Day10.line_info(line) end)
 
     1
   end
--- advent/2021/advent/test/day_10_test.exs
+++ advent/2021/advent/test/day_10_test.exs
@@ -2,6 +2,16 @@ defmodule Day10Test do
   use ExUnit.Case
   doctest Day10
 
+  test "corrupt empty stack" do
+    {:corrupt, val} = Day10.line_info(["}"], [])
+    assert val == "}"
+  end
+
+  test "incomplete stack" do
+    {:incomplete, val} = Day10.line_info(["[", "{", "<"], [])
+    assert val == 3
+  end
+
   test "day 10, part 1" do
     assert Day10.part1() == 1
   end
