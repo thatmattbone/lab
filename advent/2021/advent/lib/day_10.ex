6007fe8a4ef9c1ddbca02810438d65db0793522b
Matt Bone <thatmattbone@gmail.com>
2021-12-15 19:54:16 -0600

    checkpoint parsing day 10

--- advent/2021/advent/lib/day_10.ex
+++ advent/2021/advent/lib/day_10.ex
@@ -1,5 +1,10 @@
 defmodule Day10 do
   def part1() do
+    File.read!("input/input_10")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> String.split(line, "", trim: true) end)
+      |> IO.inspect()
+
     1
   end
 
