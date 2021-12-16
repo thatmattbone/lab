7032244c3f39b92e30ac4d466df949fc2b0944fb
Matt Bone <thatmattbone@gmail.com>
2021-12-15 22:18:35 -0600

    parsing for day 11

--- advent/2021/advent/lib/day_11.ex
+++ advent/2021/advent/lib/day_11.ex
@@ -1,5 +1,25 @@
 defmodule Day11 do
+  def parse_grid() do
+    grid = File.read!("input/input_11")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+          Enum.map(String.split(line, "", trim: true), fn char -> String.to_integer(char) end)
+            |> List.to_tuple()
+        end)
+      |> List.to_tuple()
+      |> IO.inspect()
+
+    map = for x <- 0..9,
+              y <- 0..9,
+              into: %{} do
+                {{x, y}, grid |> elem(y) |> elem(x)}
+              end
+
+    map
+  end
+
   def part1() do
+    IO.inspect(parse_grid(), limit: :infinity)
     1
   end
 
