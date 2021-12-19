ff826661daa2bfdcdee639dc812897b81c459f2b
Matt Bone <thatmattbone@gmail.com>
2021-12-19 17:44:31 -0600

    stub out day 12

---
+++ advent/2021/advent/test/day_12_test.exs
@@ -0,0 +1,12 @@
+defmodule Day12Test do
+  use ExUnit.Case
+  doctest Day12
+
+  test "day 12, part 1" do
+    assert Day12.part1() == 1
+  end
+
+  test "day 12, part 2" do
+    assert Day12.part2() == 2
+  end
+end
