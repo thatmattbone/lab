07c9c2c6e70d64a0d594bfd01932fdb752700d19
Matt Bone <thatmattbone@gmail.com>
2021-12-20 18:30:02 -0600

    parse day 12 input

--- advent/2021/advent/lib/day_12.ex
+++ advent/2021/advent/lib/day_12.ex
@@ -1,5 +1,17 @@
 defmodule Day12 do
+
+  def parse_input_to_map() do
+    File.read!("input/input_12")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+          [start_elem, end_elem] = String.split(line, "-")
+          {start_elem, end_elem}
+        end)
+      |> Map.new()
+  end
   def part1() do
+    input_map = parse_input_to_map()
+    IO.inspect(input_map)
     1
   end
 
