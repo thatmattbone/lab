ccb3d1ecda5dea49cbdc6e2bc96ab5f8cc45807b
Matt Bone <thatmattbone@gmail.com>
2021-12-31 15:35:42 -0600

    checkpoint some typespecs

--- advent/2021/advent/lib/day_12.ex
+++ advent/2021/advent/lib/day_12.ex
@@ -1,5 +1,9 @@
 defmodule Day12 do
+  @type cave_description() :: [{String.t(), String.t()}]
+  @type path_list() :: [String.t()]
 
+
+  @spec parse_input :: cave_description()
   def parse_input do
     File.read!("input/input_12")
       |> String.split("\n", trim: true)
@@ -9,10 +13,12 @@ defmodule Day12 do
         end)
   end
 
+  @spec is_big_cave?(String.t()) :: boolean
   def is_big_cave?(cave_name) do
     String.upcase(cave_name) == cave_name
   end
 
+  @spec get_next_paths(cave_description(), String.t()) :: path_list()
   def get_next_paths(paths, curr_elem) do
     paths
      |> Enum.filter(fn {src, _dest} ->
@@ -23,6 +29,7 @@ defmodule Day12 do
         end)
   end
 
+  @spec filter_next_paths(path_list(), path_list()) :: path_list()
   def filter_next_paths(next_paths, curr_path) do
     Enum.filter(next_paths, fn elem ->
       if Enum.member?(curr_path, elem) do
@@ -33,32 +40,35 @@ defmodule Day12 do
     end)
   end
 
-  def find_paths(paths, curr_elem, curr_path) do
-    next_paths = get_next_paths(paths, curr_elem) |> filter_next_paths(curr_path)
+  @spec find_paths(cave_description(), path_list()) :: [path_list()]
+  def find_paths(paths, [curr_elem | curr_path]) do
+    next_paths = get_next_paths(paths, curr_elem) |> filter_next_paths([curr_elem | curr_path])
 
-    #IO.inspect(curr_elem)
-    #IO.inspect(next_paths)
-    #2require IEx; IEx.pry()
+    IO.inspect(curr_elem)
+    IO.inspect(next_paths)
+    require IEx; IEx.pry()
 
     if length(next_paths) == 0 do
       [curr_path]
     else
       Enum.map(next_paths, fn next_elem ->
-        new_curr_path = [curr_elem | curr_path] ++ [next_elem]
+        new_curr_path = [next_elem | curr_path]
 
-        find_paths(paths, next_elem, new_curr_path)
+        find_paths(paths, new_curr_path)
       end)
     end
   end
 
+  @spec part1 :: integer()
   def part1() do
     input_list = parse_input()
 
-    find_paths(input_list, "start", [])
+    find_paths(input_list, ["start"])
       |> IO.inspect()
     1
   end
 
+  @spec part2 :: integer()
   def part2() do
     2
   end
