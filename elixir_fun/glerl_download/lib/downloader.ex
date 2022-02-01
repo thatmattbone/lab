505a002ae5ce7d785248ba6cf1e5772dc442a553
Matt Bone <thatmattbone@gmail.com>
2022-01-31 21:43:18 -0600

    can download per-year content

--- elixir_fun/glerl_download/lib/downloader.ex
+++ elixir_fun/glerl_download/lib/downloader.ex
@@ -1,5 +1,8 @@
 defmodule  Downloader do
 
+  @min_year 2000
+  @max_year 2022
+
   @spec get_data_dir :: String.t()
   def get_data_dir() do
     "data"
@@ -12,7 +15,23 @@ defmodule  Downloader do
   end
 
   @spec filename_from_year(integer()) :: String.t()
-  def filename_from_year(year) when year >= 2001 and year < 2022 do
+  def filename_from_year(year) when year >= @min_year and year < @max_year do
     "chi#{year}.04t.txt"
+  end
+
+  @spec url_from_year(integer()) :: String.t()
+  def url_from_year(year) when year >= @min_year and year < @max_year do
+    filename = filename_from_year(year)
+
+    "https://www.glerl.noaa.gov/metdata/chi/archive/#{filename}"
+  end
+
+  @spec fetch_file_for_year(integer()) :: nil
+  def fetch_file_for_year(year) when year >= @min_year and year < @max_year do
+    :ssl.start()
+    :inets.start()
+
+    {:ok, {_status, _headers, response_content}} = :httpc.request(url_from_year(year))
+    File.write(get_data_dir() <> "/" <> filename_from_year(year), response_content)
   end
 end
