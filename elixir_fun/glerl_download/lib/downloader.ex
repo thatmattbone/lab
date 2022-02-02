8454cd12b78f1acb3f6f81f4dd8d70f62fe87b48
Matt Bone <thatmattbone@gmail.com>
2022-02-02 09:20:47 -0600

    checkpoint a few things from a couple days ago

--- elixir_fun/glerl_download/lib/downloader.ex
+++ elixir_fun/glerl_download/lib/downloader.ex
@@ -14,16 +14,21 @@ defmodule  Downloader do
     end
   end
 
-  @spec filename_from_year(integer()) :: String.t()
-  def filename_from_year(year) when year >= @min_year and year < @max_year do
+  @spec filename_for_year(integer()) :: String.t()
+  def filename_for_year(year) when year >= @min_year and year < @max_year do
     "chi#{year}.04t.txt"
   end
 
-  @spec url_from_year(integer()) :: String.t()
-  def url_from_year(year) when year >= @min_year and year < @max_year do
-    filename = filename_from_year(year)
+  @spec url_for_year(integer()) :: String.t()
+  def url_for_year(year) when year >= @min_year and year < @max_year do
+    filename = filename_for_year(year)
 
     "https://www.glerl.noaa.gov/metdata/chi/archive/#{filename}"
+  end
+
+  @spec file_path_for_year(integer()) :: String.t()
+  def file_path_for_year(year) when year >= @min_year and year < @max_year do
+    get_data_dir() <> "/" <> filename_for_year(year)
   end
 
   @spec fetch_file_for_year(integer()) :: nil
@@ -31,7 +36,9 @@ defmodule  Downloader do
     :ssl.start()
     :inets.start()
 
-    {:ok, {_status, _headers, response_content}} = :httpc.request(url_from_year(year))
-    File.write(get_data_dir() <> "/" <> filename_from_year(year), response_content)
+    {:ok, {_status, _headers, response_content}} = :httpc.request(url_for_year(year))
+    File.write(file_path_for_year(year), response_content)
+
+    nil
   end
 end
