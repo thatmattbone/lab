889cbdbfdd9549d9285c8c55cba9525ac8e6b473
Matt Bone <thatmattbone@gmail.com>
2022-02-06 10:15:47 -0600

    parse to typed lists works

--- elixir_fun/glerl_download/lib/data_parser.ex
+++ elixir_fun/glerl_download/lib/data_parser.ex
@@ -1,7 +1,7 @@
 defmodule DataParser do
 
-  def parse(input_str) do
-    [_head1, _head2 | lines] = String.split(input_str, "\n")
+  def input_str_to_lines(input_str) do
+    [_head1, _head2 | lines] = String.trim(input_str) |> String.split("\n")
 
     lines
       |> Enum.map(fn line ->
@@ -9,5 +9,29 @@ defmodule DataParser do
           |> String.split(" ")
           |> Enum.filter(fn item -> String.length(item) > 0 end)
       end)
+      |> Enum.filter(fn [item1 | _rest] -> item1 == "4" end)
+  end
+
+  def parse_utc(utc_str) do
+    String.to_integer(utc_str)  # TODO do more with this
+  end
+
+  def line_to_typed_line([id, year, doy, utc, temp_c, speed, gusts, direction]) do
+    [
+      String.to_integer(id),
+      String.to_integer(year),
+      String.to_integer(doy),
+      parse_utc(utc),
+      String.to_float(temp_c),
+      String.to_float(speed),
+      String.to_float(gusts),
+      String.to_integer(direction)
+    ]
+  end
+
+  def parse(input_str) do
+    input_str_to_lines(input_str)
+      #|> IO.inspect(limit: :infinity)
+      |> Enum.map(&line_to_typed_line/1)
   end
 end
--- elixir_fun/glerl_download/lib/mix/tasks/glerl.ex
+++ elixir_fun/glerl_download/lib/mix/tasks/glerl.ex
@@ -1,8 +1,7 @@
 defmodule Mix.Tasks.Glerl do
   def run(_) do
     result = ArchiveDownloader.read_file_for_year(2011)
-    IO.inspect(result)
-    IO.puts("fuck")
+
     IO.inspect(DataParser.parse(result))
   end
 end
