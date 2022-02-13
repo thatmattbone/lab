157f33827b8b13c92f1ad6b73a78c8f51e190847
Matt Bone <thatmattbone@gmail.com>
2022-02-12 22:17:47 -0600

    stubbed out poller seems to work

--- elixir_fun/glerl_download/lib/poller.ex
+++ elixir_fun/glerl_download/lib/poller.ex
@@ -2,9 +2,30 @@ defmodule GlerlPoller do
   use GenServer
 
   def start_link(opts) do
-    IO.puts("fuck")
+    IO.puts("GlerlPoller.start_link/1")
     IO.inspect(opts)
     #server = Keyword.fetch!(opts, :name)
-    GenServer.start_link(__MODULE__, :foo, opts)
+    GenServer.start_link(__MODULE__, __MODULE__, opts)
+  end
+
+  def init(init_arg) do
+    IO.puts("GlerlPoller.init/1")
+
+    schedule_work()
+
+    {:ok, init_arg}
+  end
+
+  def handle_info(:poll, state) do
+    IO.puts("...doing my polling work...")
+
+    schedule_work()
+
+    {:noreply, state}
+  end
+
+  defp schedule_work() do
+    # 2 * 60 * 60 * 1000 # In 2 hours
+    Process.send_after(self(), :poll, 10 * 1000)
   end
 end
