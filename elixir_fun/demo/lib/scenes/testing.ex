321ce11bd3cc6647badbb848d84781e6adac0553
Matt Bone <thatmattbone@gmail.com>
2022-03-20 18:20:17 -0500

    hook the nav and notes up to my stubbed out testing module

--- elixir_fun/demo/lib/scenes/testing.ex
+++ elixir_fun/demo/lib/scenes/testing.ex
@@ -5,6 +5,13 @@ defmodule Demo.Scene.Testing do
 
   import Scenic.Primitives
 
+  alias Demo.Component.Nav
+  alias Demo.Component.Notes
+
+  @notes """
+  ...these are some notes...
+  """
+
   @impl Scenic.Scene
   def init(scene, _param_, _opts) do
     {width, height} = scene.viewport.size
@@ -15,6 +22,9 @@ defmodule Demo.Scene.Testing do
     graph = Graph.build(font: :roboto, font_size: 24)
       |> text("Hello World", text_align: :center, translate: {300, 300})
       |> circle(100, stroke: {2, :green}, translate: {300, 300})
+
+      |> Nav.add_to_graph(__MODULE__)
+      |> Notes.add_to_graph(@notes)
 
     scene =
       scene
