23929970c1ad1721e3a1f860ba48346368e593a8
Matt Bone <thatmattbone@gmail.com>
2022-03-28 21:46:08 -0500

    stub out more of the ring datastruct

--- elixir_fun/glerl_download/lib/ring.ex
+++ elixir_fun/glerl_download/lib/ring.ex
@@ -1,12 +1,28 @@
 defmodule Ring do
+  defstruct [
+    :data_map,
+    :current_position,
+    :length
+  ]
 
-  def push([], obj) do
-    [obj]
+
+  def new(size) do
+    %Ring{
+      data_map: (for i <- 0..(size - 1), into: %{}, do: {i, nil}),
+      current_position: 0,
+      length: size
+    }
   end
 
 
-  def push(ring_list, obj) do
+  def push(ring = %Ring{}, obj) do
+    IO.inspect(ring.current_position)
+    IO.inspect(ring.length)
+    IO.inspect(ring.data_map)
 
+    IO.inspect(obj)
+    
+    ring
   end
 
 end
