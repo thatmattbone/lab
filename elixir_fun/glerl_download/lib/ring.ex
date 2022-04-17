002a3885fd855259d4a1d038af76328d468b7f28
Matt Bone <thatmattbone@gmail.com>
2022-04-17 17:34:12 -0500

    make Ring.fix_index private

--- elixir_fun/glerl_download/lib/ring.ex
+++ elixir_fun/glerl_download/lib/ring.ex
@@ -22,24 +22,23 @@ defmodule Ring do
     }
   end
 
-  def fix_index(index, size) when index < size do  # TODO make this private? why didn't that work?
+  defp fix_index(index, size) when index < size do  # TODO make this private? why didn't that work?
     index
   end
 
-  def fix_index(index, size) do
+  defp fix_index(index, size) do
     index - size
   end
 
   def to_list(ring = %Ring{}) do
-    # should probably reverse this??
     for i <- ring.current_position..(ring.current_position + ring.length - 1) do
-      fixed_i = Ring.fix_index(i, ring.length)
+      fixed_i = fix_index(i, ring.length)
       Map.get(ring.data_map, fixed_i)
     end
   end
 
 end
 
-defimpl Enumerable, for: Ring do
-
-end
+# defimpl Enumerable, for: Ring do
+#
+# end
