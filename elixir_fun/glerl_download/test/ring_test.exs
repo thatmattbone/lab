adf2bea48f81f0c55a4e56892792252b69997a59
Matt Bone <thatmattbone@gmail.com>
2022-04-17 17:32:30 -0500

    more ring tests

--- elixir_fun/glerl_download/test/ring_test.exs
+++ elixir_fun/glerl_download/test/ring_test.exs
@@ -14,4 +14,42 @@ defmodule RingTest do
       4 => nil
     }
   end
+
+  test "push ring" do
+    ring = Ring.new(3)
+
+    ring = Ring.push(ring, "one")
+
+    assert ring.current_position == 1
+    assert ring.data_map == %{
+      0 => "one",
+      1 => nil,
+      2 => nil
+    }
+
+    ring = ring
+      |> Ring.push("two")
+      |> Ring.push("three")
+      |> Ring.push("four")
+
+    assert ring.current_position == 1
+    assert ring.data_map == %{
+      0 => "four",
+      1 => "two",
+      2 => "three"
+    }
+  end
+
+  test "to list" do
+    ring = Ring.new(3)
+      |> Ring.push("a")
+      |> Ring.push("b")
+      |> Ring.push("c")
+
+    assert Ring.to_list(ring) == ["a", "b", "c"]
+
+    ring = ring |> Ring.push("d") |> Ring.push("e")
+
+    assert Ring.to_list(ring) == ["c", "d", "e"]
+  end
 end
