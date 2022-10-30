3578d0d128a669f87ed5d2f16f1cc9ba99286c1a
Matt Bone <thatmattbone@gmail.com>
2022-10-30 18:33:21 -0500

    word list and random word function

--- elixir_fun/hangman/dictionary/lib/dictionary.ex
+++ elixir_fun/hangman/dictionary/lib/dictionary.ex
@@ -1,18 +1,10 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for `Dictionary`.
-  """
 
-  @doc """
-  Hello world.
+  def word_list do
+    File.read!("assets/words.txt") |> String.split(~r/\n/, trim: true)
+  end
 
-  ## Examples
-
-      iex> Dictionary.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def random_word do
+    word_list() |> Enum.random()
   end
 end
