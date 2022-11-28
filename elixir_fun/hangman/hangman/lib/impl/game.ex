7dee1a540ba48478c82277f7b6345c754f37affa
Matt Bone <thatmattbone@gmail.com>
2022-11-27 23:04:48 -0600

    fix the dictionary file reading and hookup the first stab at the struct/random word generation stuff

---
+++ elixir_fun/hangman/hangman/lib/impl/game.ex
@@ -0,0 +1,16 @@
+defmodule Hangman.Impl.Game do
+
+  defstruct(
+    turns_left: 7,
+    game_state: :initialized,
+    letters: [],
+    used: MapSet.new(),
+  )
+
+  def new_game() do
+    %Hangman.Impl.Game{
+      letters: Dictionary.random_word() |> String.codepoints()
+    }
+  end
+
+end
