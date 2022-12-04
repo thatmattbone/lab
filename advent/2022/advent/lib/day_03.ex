b722fc846a8a96c730b0dfca201c3a1b9a350e32
Matt Bone <thatmattbone@gmail.com>
2022-12-04 16:49:18 -0600

    input splitting for day 3, part 1

--- advent/2022/advent/lib/day_03.ex
+++ advent/2022/advent/lib/day_03.ex
@@ -1,3 +1,10 @@
 defmodule Day03 do
-  
+  def part1() do
+    File.read!("input/input_03")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        half_len = String.length(line) / 2 |> trunc()
+        [String.slice(line, 0, half_len), String.slice(line, half_len, half_len * 2)]
+      end)
+  end
 end
