a5f8b7f721921857fed926d5e9727deb9eca04d2
Matt Bone <thatmattbone@gmail.com>
2022-12-04 12:29:58 -0600

    add day 1, part 2

--- advent/2022/advent/lib/day_01.ex
+++ advent/2022/advent/lib/day_01.ex
@@ -1,14 +1,22 @@
 defmodule Day01 do
 
-  def part1() do
+  def elf_bucket_sums() do
     elf_buckets = File.read!("input/input_01")
       |> String.split("\n\n")
       |> Enum.map(fn x ->
         x |> String.split() |> Enum.map(&String.to_integer/1)
       end)
 
-    elf_bucket_sums = Enum.map(elf_buckets, fn x -> Enum.sum(x) end)
+    Enum.map(elf_buckets, fn x -> Enum.sum(x) end)
+  end
 
-    Enum.max(elf_bucket_sums)
+  def part1() do
+    Enum.max(elf_bucket_sums())
+  end
+
+  def part2() do
+    Enum.sort(elf_bucket_sums(), :desc)
+      |> Enum.slice(0, 3)
+      |> Enum.sum()
   end
 end
--- advent/2022/advent/test/day_01_test.exs
+++ advent/2022/advent/test/day_01_test.exs
@@ -4,4 +4,8 @@ defmodule Day01Test do
   test "day 01, part 1" do
     assert Day01.part1() == 69836
   end
+
+  test "day 01, part 2" do
+    assert Day01.part2() == 207968
+  end
 end
