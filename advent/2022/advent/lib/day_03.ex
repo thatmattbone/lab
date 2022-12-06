33e6e5f1f591b4efcd2364b539e51149468dbef7
Matt Bone <thatmattbone@gmail.com>
2022-12-06 09:00:07 -0600

    more work on day 3, part 1

--- advent/2022/advent/lib/day_03.ex
+++ advent/2022/advent/lib/day_03.ex
@@ -1,4 +1,22 @@
 defmodule Day03 do
+
+
+  # Lowercase item types a through z have priorities 1 through 26.
+  # Uppercase item types A through Z have priorities 27 through 52.
+
+  def priority(input_str) do
+
+  end
+
+  def add_to_count_map(count_map, str) do
+    Map.update(count_map, str, [str], fn existing_list -> existing_list ++ [str] end)
+  end
+
+  def str_to_count_map(input_str) do
+    String.split(input_str, "", trim: true)
+      |> Enum.reduce(%{}, fn i, count_map -> add_to_count_map(count_map, i) end)
+  end
+
   def part1() do
     File.read!("input/input_03")
       |> String.split("\n", trim: true)
