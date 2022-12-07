ca28a28383eec728ade213c590336058904c4f84
Matt Bone <thatmattbone@gmail.com>
2022-12-07 07:43:53 -0600

    day 4, skeleton for part 2

--- advent/2022/advent/lib/day_04.ex
+++ advent/2022/advent/lib/day_04.ex
@@ -1,5 +1,16 @@
 defmodule Day04 do
 
+  def line_to_elf_tuples(line) do
+    [first_elf, second_elf] = String.split(line, ",")
+
+    [[e1_start_str, e1_end_str], [e2_start_str, e2_end_str]] = [String.split(first_elf, "-"), String.split(second_elf, "-")]
+
+    {
+      {String.to_integer(e1_start_str), String.to_integer(e1_end_str)},
+      {String.to_integer(e2_start_str), String.to_integer(e2_end_str)},
+    }
+  end
+
   def is_total_overlap({{e1_start, e1_end}, {e2_start, e2_end}}) when e1_start >= e2_start and e1_end <= e2_end do
     true
   end
@@ -12,23 +23,28 @@ defmodule Day04 do
     false
   end
 
+  def calc_overlap({{e1_start, e1_end}, {e2_start, e2_end}}) do
+    1
+  end
+
   def part1() do
     input = File.read!("input/input_04")
       |> String.split("\n", trim: true)
-      |> Enum.map(fn line ->
-        [first_elf, second_elf] = String.split(line, ",")
+      |> Enum.map(&line_to_elf_tuples/1)
 
-        [[e1_start_str, e1_end_str], [e2_start_str, e2_end_str]] = [String.split(first_elf, "-"), String.split(second_elf, "-")]
+    input
+      |> Enum.filter(&is_total_overlap/1)
+      |> length()
+  end
 
-        {
-          {String.to_integer(e1_start_str), String.to_integer(e1_end_str)},
-          {String.to_integer(e2_start_str), String.to_integer(e2_end_str)},
-        }
-      end)
+  def part2() do
+    input = File.read!("input/input_04")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&line_to_elf_tuples/1)
 
-      input
-        |> Enum.filter(&is_total_overlap/1)
-        |> length()
+    input
+      |> Enum.map(&calc_overlap/1)
+      |> Enum.sum()
   end
 
 end
