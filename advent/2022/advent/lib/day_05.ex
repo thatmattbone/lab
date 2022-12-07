d225d85c42c1271bf731a9f04b51c1c3590a2b38
Matt Bone <thatmattbone@gmail.com>
2022-12-07 09:25:00 -0600

    day 5, part 1 more work

--- advent/2022/advent/lib/day_05.ex
+++ advent/2022/advent/lib/day_05.ex
@@ -1,5 +1,36 @@
 defmodule Day05 do
 
+  def read_starting_stack() do
+    File.read!("input/input_05")
+      |> String.split("\n", trim: true)
+      |> Enum.slice(0, 9)
+  end
+
+  def starting_stack() do
+    # this is hardcoded for now
+    %{
+      1 => String.split("NWFRZSMD", "", trim: true),
+      2 => String.split("SGQPW", "", trim: true),
+      3 => String.split("CJNFQVRW", "", trim: true),
+      4 => String.split("LDGCPZF", "", trim: true),
+      5 => String.split("SPT", "", trim: true),
+      6 => String.split("LRWFDH", "", trim: true),
+      7 => String.split("CDNZ", "", trim: true),
+      8 => String.split("QJSVFRNW", "", trim: true),
+      9 => String.split("VWZGSMR", "", trim: true),
+    }
+  end
+
+  def move(stack, count, from, to) do
+
+  end
+
+  def read_instructions() do
+    File.read!("input/input_05")
+      |> String.split("\n", trim: true)
+      |> Enum.drop(9)
+  end
+
   def part1() do
 
   end
