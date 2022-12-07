7991f46bbf3ce66ddd869252014a171314987296
Matt Bone <thatmattbone@gmail.com>
2022-12-07 07:30:56 -0600

    day 4, part 1

--- advent/2022/advent/lib/day_04.ex
+++ advent/2022/advent/lib/day_04.ex
@@ -1,7 +1,34 @@
 defmodule Day04 do
 
-  def part1() do
+  def is_total_overlap({{e1_start, e1_end}, {e2_start, e2_end}}) when e1_start >= e2_start and e1_end <= e2_end do
+    true
+  end
 
+  def is_total_overlap({{e1_start, e1_end}, {e2_start, e2_end}}) when e2_start >= e1_start and e2_end <= e1_end do
+    true
+  end
+
+  def is_total_overlap({{e1_start, e1_end}, {e2_start, e2_end}}) do
+    false
+  end
+
+  def part1() do
+    input = File.read!("input/input_04")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [first_elf, second_elf] = String.split(line, ",")
+
+        [[e1_start_str, e1_end_str], [e2_start_str, e2_end_str]] = [String.split(first_elf, "-"), String.split(second_elf, "-")]
+
+        {
+          {String.to_integer(e1_start_str), String.to_integer(e1_end_str)},
+          {String.to_integer(e2_start_str), String.to_integer(e2_end_str)},
+        }
+      end)
+
+      input
+        |> Enum.filter(&is_total_overlap/1)
+        |> length()
   end
 
 end
--- advent/2022/advent/test/day_04_test.exs
+++ advent/2022/advent/test/day_04_test.exs
@@ -2,7 +2,10 @@ defmodule Day04Test do
   use ExUnit.Case
 
   test "day 04, part 1" do
-
+    assert Day04.part1() == 475
   end
 
+  test "day 04, part 2" do
+
+  end
 end
