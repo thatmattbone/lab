23cd2b4b33fa7c80e9b039cef65af8174c9f6fb7
Matt Bone <thatmattbone@gmail.com>
2022-12-07 15:29:28 -0600

    day 5, part 2

--- advent/2022/advent/lib/day_05.ex
+++ advent/2022/advent/lib/day_05.ex
@@ -38,25 +38,51 @@ defmodule Day05 do
     move(move(stack, from, to), from, to, count - 1)
   end
 
+  def move_and_retain(stack, from, to, count) do
+    from_stack = Map.get(stack, from)
+
+    boxes_moved = Enum.slice(from_stack, 0, count)
+
+    new_from_stack = Enum.drop(from_stack, count)
+
+    new_to_stack = boxes_moved ++ Map.get(stack, to)
+
+    stack
+      |> Map.put(from, new_from_stack)
+      |> Map.put(to, new_to_stack)
+  end
+
   def read_instructions() do
     File.read!("input/input_05")
       |> String.split("\n", trim: true)
       |> Enum.drop(9)
   end
 
-  def part1() do
+  def parse_instructions() do
     instructions = read_instructions()
       |> Enum.map(fn line ->
         [_, count, _, from, _, to] = String.split(line, " ", trim: true)
         {String.to_integer(count), String.to_integer(from), String.to_integer(to)}
       end)
+  end
 
-    final_stack = Enum.reduce(instructions, starting_stack(), fn {count, from, to}, stack -> move(stack, from, to, count) end)
-
+  def top_of_stack(final_stack) do
     Enum.map(1..9, fn x ->
       [head | rest] = Map.get(final_stack, x)
       head
     end) |> Enum.join("")
+  end
+
+  def part1() do
+    parse_instructions()
+      |> Enum.reduce(starting_stack(), fn {count, from, to}, stack -> move(stack, from, to, count) end)
+      |> top_of_stack()
+  end
+
+  def part2() do
+    parse_instructions()
+      |> Enum.reduce(starting_stack(), fn {count, from, to}, stack -> move_and_retain(stack, from, to, count) end)
+      |> top_of_stack()
   end
 
 end
--- advent/2022/advent/test/day_05_test.exs
+++ advent/2022/advent/test/day_05_test.exs
@@ -38,8 +38,33 @@ defmodule Day05Test do
     }
   end
 
+  test "move_and_retain" do
+    start_map = %{
+      1 => ["A", "B"],
+      2 => ["C"],
+      3 => ["D", "E"],
+    }
+    next_map = Day05.move_and_retain(start_map, 1, 3, 1)
+
+    assert next_map == %{
+      1 => ["B"],
+      2 => ["C"],
+      3 => ["A", "D", "E"],
+    }
+
+    third_map = Day05.move_and_retain(next_map, 3, 1, 3)
+    assert third_map == %{
+      1 => ["A", "D", "E", "B"],
+      2 => ["C"],
+      3 => [],
+    }
+  end
+
   test "day 05, part 1" do
     assert Day05.part1() == "FWNSHLDNZ"
   end
 
+  test "day 05, part 2" do
+    assert Day05.part2() == "RNRGDNFQG"
+  end
 end
