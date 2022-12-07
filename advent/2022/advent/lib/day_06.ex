e767c0aa10fc50af3460f09f901ce1b94e301d48
Matt Bone <thatmattbone@gmail.com>
2022-12-07 15:56:55 -0600

    day 6, part 1

--- advent/2022/advent/lib/day_06.ex
+++ advent/2022/advent/lib/day_06.ex
@@ -1,7 +1,19 @@
 defmodule Day06 do
 
-  def part1() do
+  def find_start([one, two, three, four | rest], count) do
+    uniq = MapSet.new([one, two, three, four]) |> MapSet.size()
 
+    if uniq == 4 do
+      count
+    else
+      find_start([two, three, four | rest], count + 1)
+    end
+  end
+
+  def part1() do
+    File.read!("input/input_06")
+      |> String.split("", trim: true)
+      |> find_start(4)
   end
 
   def part2() do
--- advent/2022/advent/test/day_06_test.exs
+++ advent/2022/advent/test/day_06_test.exs
@@ -1,8 +1,16 @@
 defmodule Day06Test do
   use ExUnit.Case
 
-  test "day 06, part1" do
+  test "find_start()" do
+    Day06.find_start(String.split("bvwbjplbgvbhsrlpgdmjqwftvncz", "", trim: true), 4) == 5
+    Day06.find_start(String.split("nppdvjthqldpwncqszvftbrmjlhg", "", trim: true), 4) == 6
+    Day06.find_start(String.split("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "", trim: true), 4) == 10
+    Day06.find_start(String.split("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "", trim: true), 4) == 11
+  end
 
+
+  test "day 06, part1" do
+    Day06.part1() == 1566
   end
 
   test "day 06, part2" do
