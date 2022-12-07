9128bb03e390d2cbb7e302bbcfaf9f233a71e15f
Matt Bone <thatmattbone@gmail.com>
2022-12-07 16:16:37 -0600

    day 6, part 2 tests for real

--- advent/2022/advent/test/day_06_test.exs
+++ advent/2022/advent/test/day_06_test.exs
@@ -2,25 +2,25 @@ defmodule Day06Test do
   use ExUnit.Case
 
   test "find_start()" do
-    Day06.find_start(String.split("bvwbjplbgvbhsrlpgdmjqwftvncz", "", trim: true), 4) == 5
-    Day06.find_start(String.split("nppdvjthqldpwncqszvftbrmjlhg", "", trim: true), 4) == 6
-    Day06.find_start(String.split("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "", trim: true), 4) == 10
-    Day06.find_start(String.split("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "", trim: true), 4) == 11
+    assert Day06.find_start(String.split("bvwbjplbgvbhsrlpgdmjqwftvncz", "", trim: true), 4) == 5
+    assert Day06.find_start(String.split("nppdvjthqldpwncqszvftbrmjlhg", "", trim: true), 4) == 6
+    assert Day06.find_start(String.split("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "", trim: true), 4) == 10
+    assert Day06.find_start(String.split("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "", trim: true), 4) == 11
   end
 
   test "find_start_14()" do
-    Day06.find_start_14(String.split("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "", trim: true), 14) == 19
-    Day06.find_start_14(String.split("bvwbjplbgvbhsrlpgdmjqwftvncz", "", trim: true), 14) == 23
-    Day06.find_start_14(String.split("nppdvjthqldpwncqszvftbrmjlhg", "", trim: true), 14) == 23
-    Day06.find_start_14(String.split("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "", trim: true), 14) == 26
+    assert Day06.find_start_14(String.split("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "", trim: true), 14) == 19
+    assert Day06.find_start_14(String.split("bvwbjplbgvbhsrlpgdmjqwftvncz", "", trim: true), 14) == 23
+    assert Day06.find_start_14(String.split("nppdvjthqldpwncqszvftbrmjlhg", "", trim: true), 14) == 23
+    assert Day06.find_start_14(String.split("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "", trim: true), 14) == 26
   end
 
   test "day 06, part1" do
-    Day06.part1() == 1566
+    assert Day06.part1() == 1566
   end
 
   test "day 06, part2" do
-
+    assert Day06.part2()
   end
 
 end
