f8f32b803053db989c2e4a9a993ce82d4dd2d506
Matt Bone <thatmattbone@gmail.com>
2022-12-09 23:27:36 -0600

    parse day 9 input file

--- advent/2022/advent/lib/day_09.ex
+++ advent/2022/advent/lib/day_09.ex
@@ -1,7 +1,17 @@
 defmodule Day09 do
 
-  def part1() do
+  def parse_direction(d) when d == "U", do: :up
+  def parse_direction(d) when d == "D", do: :down
+  def parse_direction(d) when d == "R", do: :right
+  def parse_direction(d) when d == "L", do: :left
 
+  def part1() do
+    File.read!("input/input_09")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [d, magnitude] = String.split(line, " ")
+        {parse_direction(d), String.to_integer(magnitude)}
+      end)
   end
 
 
