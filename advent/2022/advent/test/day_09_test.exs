99b21d8ead0c5f3e98ca58610dbe63cb02a46b0a
Matt Bone <thatmattbone@gmail.com>
2022-12-09 23:17:59 -0600

    stub out day 9

---
+++ advent/2022/advent/test/day_09_test.exs
@@ -0,0 +1,11 @@
+defmodule Day09Test do
+  use ExUnit.Case
+
+  test "day 09, part 1" do
+    assert Day09.part1() == nil
+  end
+
+  test "day 09, part 2" do
+    assert Day09.part2() == nil
+  end
+end
