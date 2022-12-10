ae126803beb7b38e64a08baa22f79d59e9253d7a
Matt Bone <thatmattbone@gmail.com>
2022-12-10 10:23:13 -0600

    day 9, stub out first attempt at the tail move list

--- advent/2022/advent/lib/day_09.ex
+++ advent/2022/advent/lib/day_09.ex
@@ -36,6 +36,92 @@ defmodule Day09 do
     [{0, 0}] ++ move_list(moves, {0, 0})
   end
 
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx and ty == hy do
+    # overlap
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx + 1 and ty == hy do
+    # one to right
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx - 1 and ty == hy do
+    # one to left
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx and ty == hy - 1 do
+    # one down
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx and ty == hy + 1 do
+    # one up
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx + 1 and ty == hy + 1 do
+    # upper right
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx - 1 and ty == hy + 1 do
+    # upper left
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx + 1 and ty == hy - 1 do
+    # lower right
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx - 1 and ty == hy - 1 do
+    # lower left
+    {tx, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx + 2 and ty == hy do
+    # two to right
+    {tx + 1, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx - 2 and ty == hy do
+    # two to left
+    {tx - 1, ty}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx and ty == hy + 2 do
+    # two up
+    {tx, ty + 1}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx and ty == hy - 2 do
+    # two down
+    {tx, ty - 1}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx + 1 and ty == hy + 2 do
+    # right one, up two
+    {tx + 1, ty + 1}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx - 1 and ty == hy + 2 do
+    # left one, up two
+    {tx - 1, ty + 1}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx + 1 and ty == hy - 2 do
+    # right one, down two
+    {tx + 1, ty - 1}
+  end
+
+  def next_tail_pos(_cur_pos={tx, ty}, _head_pos={hx, hy}) when tx == hx - 1 and ty == hy - 2 do
+    # left one, down two
+    {tx - 1, ty - 1}
+  end
+
+
   def part1() do
     moves = File.read!("input/input_09")
       |> String.split("\n", trim: true)
