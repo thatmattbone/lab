d256d14d10941545870c51b3dcb069eab64c375b
Matt Bone <thatmattbone@gmail.com>
2022-12-11 15:14:23 -0600

    checkpoint a failed attempt at part 2, now going to cheat

--- advent/2022/advent/lib/day_11.ex
+++ advent/2022/advent/lib/day_11.ex
@@ -12,6 +12,13 @@ defmodule Day11 do
           else
             1
           end
+        end,
+        :unworry => fn item ->
+          if rem(item, 7) == 0 do
+            floor(item / 7)
+          else
+            item
+          end
         end
       },
       1 => %{
@@ -23,6 +30,13 @@ defmodule Day11 do
             5
           else
             7
+          end
+        end,
+        :unworry => fn item ->
+          if rem(item, 19) == 0 do
+            floor(item / 19)
+          else
+            item
           end
         end
       },
@@ -36,6 +50,13 @@ defmodule Day11 do
           else
             3
           end
+        end,
+        :unworry => fn item ->
+          if rem(item, 13) == 0 do
+            floor(item / 13)
+          else
+            item
+          end
         end
       },
       3 => %{
@@ -47,6 +68,13 @@ defmodule Day11 do
             4
           else
             6
+          end
+        end,
+        :unworry => fn item ->
+          if rem(item, 3) == 0 do
+            floor(item / 3)
+          else
+            item
           end
         end
       },
@@ -60,6 +88,13 @@ defmodule Day11 do
           else
             6
           end
+        end,
+        :unworry => fn item ->
+          if rem(item, 2) == 0 do
+            floor(item / 2)
+          else
+            item
+          end
         end
       },
       5 => %{
@@ -71,6 +106,13 @@ defmodule Day11 do
             2
           else
             3
+          end
+        end,
+        :unworry => fn item ->
+          if rem(item, 11) == 0 do
+            floor(item / 11)
+          else
+            item
           end
         end
       },
@@ -84,6 +126,13 @@ defmodule Day11 do
           else
             1
           end
+        end,
+        :unworry => fn item ->
+          if rem(item, 17) == 0 do
+            floor(item / 17)
+          else
+            item
+          end
         end
       },
       7 => %{
@@ -96,13 +145,20 @@ defmodule Day11 do
           else
             5
           end
+        end,
+        :unworry => fn item ->
+          if rem(item, 5) == 0 do
+            floor(item / 5)
+          else
+            item
+          end
         end
         },
     }
   end
 
   def new_worry_level(level) do
-    floor(level/3)
+      floor(level/3)
   end
 
   def run_turn(monkey_map, monkey_num) do
@@ -140,8 +196,41 @@ defmodule Day11 do
     biggest * second_biggest
   end
 
-  def part2() do
+  def run_turn2(monkey_map, monkey_num) do
+    %{
+      :items => items,
+      :op => op,
+      :throw_to => throw_to,
+      :unworry => unworry,
+    } = monkey_map[monkey_num]
 
+    Enum.reduce(items, monkey_map, fn(item, monkey_map) ->
+      worry_level = op.(item)
+      throw_to_monkey = throw_to.(worry_level)
+      inspected = monkey_map[monkey_num].inspected + 1
+
+      worry_level = unworry.(worry_level)
+
+      new_item_list = monkey_map[throw_to_monkey].items ++ [worry_level]
+
+      monkey_map
+        |> put_in([monkey_num, :items], [])
+        |> put_in([monkey_num, :inspected], inspected)
+        |> put_in([throw_to_monkey, :items], new_item_list)
+    end)
   end
 
+  def run_round2(monkey_map) do
+    Enum.reduce(0..7, monkey_map, fn(item, monkey_map) ->
+      run_turn2(monkey_map, item)
+    end)
+  end
+
+  def part2() do
+    monkey_map = Enum.reduce(1..10000, build_initial_monkeys(), fn(_item, monkey_map) -> run_round2(monkey_map) end)
+
+    [biggest, second_biggest | _rest] = Enum.map(0..7, fn x -> monkey_map[x].inspected end) |> Enum.sort(:desc)
+
+    biggest * second_biggest
+  end
 end
--- advent/2022/advent/test/day_11_test.exs
+++ advent/2022/advent/test/day_11_test.exs
@@ -2,7 +2,7 @@ defmodule Day11Test do
   use ExUnit.Case
 
   test "day 11, part 1" do
-    assert Day11.part1() == nil
+    assert Day11.part1() == 58794
   end
 
   test "day 11, part 2" do
