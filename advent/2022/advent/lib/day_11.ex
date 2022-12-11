bf62f271e0cebe1c13278a39b2d59a92f4ea0686
Matt Bone <thatmattbone@gmail.com>
2022-12-11 07:57:16 -0600

    stub out day 11

---
+++ advent/2022/advent/lib/day_11.ex
@@ -0,0 +1,11 @@
+defmodule Day11 do
+
+  def part1() do
+
+  end
+
+  def part2() do
+
+  end
+
+end
---
+++ advent/2022/advent/test/day_11_test.exs
@@ -0,0 +1,11 @@
+defmodule Day11Test do
+  use ExUnit.Case
+
+  test "day 11, part 1" do
+    assert Day11.part1() == nil
+  end
+
+  test "day 11, part 2" do
+    assert Day11.part2() == nil
+  end
+end
