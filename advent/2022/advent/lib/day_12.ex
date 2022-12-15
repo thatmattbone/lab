ff355dba221550cd3c366ea3d02763a84d24e470
Matt Bone <thatmattbone@gmail.com>
2022-12-15 07:34:14 -0600

    day 12, build grid, find start and end

--- advent/2022/advent/lib/day_12.ex
+++ advent/2022/advent/lib/day_12.ex
@@ -1,7 +1,58 @@
 defmodule Day12 do
 
-  def part1() do
+  def letter_to_elevation_map() do
+    lower = for i <- 1..26, do: {to_string([i + 96]), i}
+    Map.new(lower)
+  end
 
+  def build_grid() do
+    File.read!("input/input_12")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+           String.split(line, "", trim: true) |> Enum.with_index()
+         end)
+      |> Enum.with_index()
+      |> Enum.map(fn {row, i} ->
+          Enum.map(row, fn {item, j} ->
+            {{i, j}, item}
+          end)
+         end)
+      |> List.flatten()
+      |> Map.new()
+  end
+
+  def find_start([{key, value} | _rest]) when value == "S" do
+    key
+  end
+
+  def find_start([{_key, _value} | rest]) do
+    find_start(rest)
+  end
+
+  def find_start(grid) do
+    find_start(Map.to_list(grid))
+  end
+
+  def find_end([{key, value} | _rest]) when value == "E" do
+    key
+  end
+
+  def find_end([{_key, _value} | rest]) do
+    find_end(rest)
+  end
+
+  def find_end(grid) do
+    find_end(Map.to_list(grid))
+  end
+
+  def build_nodes(grid) do
+
+  end
+
+  def part1() do
+    grid = build_grid()
+
+    build_nodes(grid)
   end
 
   def part2() do
