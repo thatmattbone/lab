3d440bfb7362271de26383b0cd0f85e41e875d72
Matt Bone <thatmattbone@gmail.com>
2023-03-14 21:56:51 -0500

    hangman genserver works

--- elixir_fun/hangman/hangman/lib/runtime/server.ex
+++ elixir_fun/hangman/hangman/lib/runtime/server.ex
@@ -1,4 +1,26 @@
 defmodule Hangman.Runtime.Server do
   use GenServer
+  alias Hangman.Impl.Game
 
+  ### client process code
+  def start_link() do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+
+  ### server process code
+  def init(_) do
+    {:ok, Game.new_game()}
+  end
+
+
+  def handle_call({:make_move, guess}, _from, game) do
+    {updated_game, tally} = game |> Game.make_move(guess)
+
+    {:reply, tally, updated_game}
+  end
+
+  def handle_call({:tally}, _from, game) do
+    {:reply, Game.tally(game), game}
+  end
 end
