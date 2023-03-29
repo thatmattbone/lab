5ffb450edc8689b803223cdf0b7ebc387a7a7153
Matt Bone <thatmattbone@gmail.com>
2023-03-28 21:39:16 -0500

    add the silly node demo

---
+++ elixir_fun/hangman/hangman/lib/node_demo.ex
@@ -0,0 +1,11 @@
+defmodule NodeDemo do
+  def reverse() do
+    receive do
+      {from_pid, msg} ->
+        IO.inspect(from_pid)
+        result = msg |> String.reverse()
+        send(from_pid, result)
+        reverse()
+    end
+  end
+end
