27ddf7ef7b6d4a80c9db2c6eaded5e2f594eb895
Matt Bone <thatmattbone@gmail.com>
2023-07-26 20:43:44 -0500

    stub out the phoenix hangman app

---
+++ elixir_fun/hangman/b1/config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :b1, B1Web.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "fb5atUo21zhH/H/HL5FZ+SZ8yQJ661lafFofrb83jbNRECv28YTkYDQtOt3V8vA4",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
