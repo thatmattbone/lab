3ab61076f61872e91ebeae7d29e69a63f443c8e6
Matt Bone <thatmattbone@gmail.com>
2023-10-25 22:38:56 -0500

    remove old glerl app (moved to separate repo)

--- elixir_fun/glerl_download/lib/conversion.ex
+++
@@ -1,5 +0,0 @@
-defmodule Conversion do
-  def meters_per_sec_to_knots(meters_per_sec) do
-    meters_per_sec * 1.943844
-  end
-end
--- elixir_fun/glerl_download/test/test_conversion.exs
+++
@@ -1,6 +0,0 @@
-defmodule ConversionTest do
-  test "meters per second to knots" do
-    assert Conversion.meters_per_sec_to_knots(5.0) == 9.71922
-    assert Conversion.meters_per_sec_to_knots(7.5) == 14.5788
-  end
-end
