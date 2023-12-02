62356bffe58f5330bd51166140c9c83e2c601462
Matt Bone <thatmattbone@gmail.com>
2023-12-01 18:45:44 -0600

    skeleton for day 02

---
+++ advent/2023/advent/day_02/prob.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from advent.util import input_path, input_lines
+
+
+def day_02_part_1(input_lines: List[str]) -> int:
+    return -1    
+
+
+def day_02_part_2(input_lines: List[str], debug: bool = False) -> int:
+    return -1
+
+
+if __name__ == '__main__':
+    input_list = input_lines(__file__)
+    print(day_02_part_1(input_list))
+    print(day_02_part_2(input_list))
