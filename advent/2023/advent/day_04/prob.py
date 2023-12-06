7df52c246876dfa474e106ebaeb3bf2093e6b629
Matt Bone <thatmattbone@gmail.com>
2023-12-05 18:31:35 -0600

    day 04, part 2 answer (but very slow)

--- advent/2023/advent/day_04/prob.py
+++ advent/2023/advent/day_04/prob.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple, Set, Dict
+from copy import copy
+from pprint import pp
 
 from advent.util import input_path, input_lines
 
@@ -36,17 +38,36 @@ def day_04_part_1(my_input: InputType, debug: bool = False) -> int:
 
 
 def day_04_part_2(my_input: InputType, debug: bool = False) -> int:
-    if debug:
-       print(my_input)
+    # if debug:
+    #    print(my_input)
 
     # breakpoint()
 
     card_count = 0
     card_stack = list(my_input.values())
-    #while len(card_stack):
-    #    pass
+    card_stack.reverse()
 
-    return -1
+    if debug:
+        pp(card_stack)
+        print('==' * 25)
+    
+    while len(card_stack) > 0:
+        card_count += 1
+        card_num, win_set, my_list = card_stack.pop()
+        wins = [i for i in my_list if i in win_set]
+        
+        for i in range(1, 1 + len(wins)):
+            card_stack.append(copy(my_input[card_num + i]))
+
+        if debug:
+            print(card_count)
+            pp(card_stack)
+            print(f'{card_num}: {win_set} | {my_list} -> {len(wins)})')
+            print('==' * 25)
+            breakpoint()
+
+            
+    return card_count
 
 
 def main():
@@ -60,13 +81,11 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""".split('\n'))
-
-    breakpoint()
     
-    print(day_04_part_1(test_input, debug=True))
+    print(day_04_part_1(test_input))
     print(day_04_part_1(my_input))
 
-    print(day_04_part_2(test_input, debug=True))
+    # print(day_04_part_2(test_input, debug=False))
     print(day_04_part_2(my_input))
     
 
