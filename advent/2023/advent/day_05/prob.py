2a79c7d001a6875675604150f29fce520b9c3280
Matt Bone <thatmattbone@gmail.com>
2023-12-06 21:58:06 -0600

    stub out input, etc

--- advent/2023/advent/day_05/prob.py
+++ advent/2023/advent/day_05/prob.py
@@ -1,24 +1,22 @@
 from typing import List
 
 from advent.util import input_path, input_lines
-from 
+from .my_input import SEEDS, SEED_TO_SOIL, SOIL_TO_FERTILIZER, FERTILIZER_TO_WATER, WATER_TO_LIGHT, LIGHT_TO_TEMPERATURE, TEMPERATURE_TO_HUMIDITY, HUMIDITY_TO_LOCATION
+#from .my_test_input import SEEDS, SEED_TO_SOIL, SOIL_TO_FERTILIZER, FERTILIZER_TO_WATER, WATER_TO_LIGHT, LIGHT_TO_TEMPERATURE, TEMPERATURE_TO_HUMIDITY, HUMIDITY_TO_LOCATION
 
 
-def day_05_part_1(input: InputType, debug: bool = False) -> int:
+def day_05_part_1(debug: bool = False) -> int:
     return -1    
 
 
-def day_05_part_2(input: InputType, debug: bool = False) -> int:
+def day_05_part_2(debug: bool = False) -> int:
     return -1
 
 
 def main():
+    print(day_05_part_1())
 
-    print(day_05_part_1(test_input, debug=True))
-    print(day_05_part_1(my_input))
-
-    print(day_05_part_2(test_input, debug=True))
-    print(day_05_part_2(my_input))
+    print(day_05_part_2())
 
         
 if __name__ == '__main__':
