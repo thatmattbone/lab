9ab5824db2ad9c6be0bad3622c5ce27cb9837a9f
Matt Bone <thatmattbone@gmail.com>
2023-12-09 15:46:45 -0600

    parsing and stuff for day 07

--- advent/2023/advent/day_07/prob.py
+++ advent/2023/advent/day_07/prob.py
@@ -1,15 +1,74 @@
-from typing import List
+from collections import Counter
+from pprint import pp
+from typing import List, Tuple
+from enum import Enum
 
 from advent.util import input_path, input_lines
 
-InputType = List[str]
 
+class HandType(Enum):
+    FIVE_OF_KIND = 700
+    FOUR_OF_KIND = 500
+    FULL_HOUSE = 600
+    THREE_OF_KIND = 400
+    TWO_PAIR = 300
+    ONE_PAIR = 200
+    HIGH_CARD = 100
+
+
+class Hand:
+    def __init__(self, hand_str: str):
+        self.hand_str = hand_str
+        self.hand_type = self.find_type(hand_str)
+
+    def find_type(self, hand_str: str) -> HandType:
+        counter = Counter(hand_str)
+        counter_val_set = set(counter.values())
+        
+        if len(counter) == 1:
+            return HandType.FIVE_OF_KIND
+
+        elif len(counter) == 2 and counter_val_set == {1, 4}:
+            return HandType.FOUR_OF_KIND
+
+        elif len(counter) == 2 and counter_val_set == {2, 3}:
+            return HandType.FULL_HOUSE
+
+        elif len(counter) == 3 and counter_val_set == {1, 3}:
+            return HandType.THREE_OF_KIND
+
+        elif len(counter) == 3 and counter_val_set == {1, 2}:
+            return HandType.TWO_PAIR
+
+        elif len(counter) == 4:
+            return HandType.ONE_PAIR
+            
+        elif len(counter) == 5:
+            return HandType.HIGH_CARD
+
+        raise ValueError(hand_str)
+
+    def __str__(self):
+        return f'{self.hand_type.name}: {self.hand_str}'
+
+    def __repr__(self):
+        return str(self)
+    
+InputType = List[Tuple[Hand, int]]
+    
 
 def build_input_type(lines: List[str]) -> InputType:
-    return lines
+    fixed_lines = []
+    for line in lines:
+        hand, bid = line.split()
+        bid = int(bid)
+        fixed_lines.append((Hand(hand), bid))
+    return fixed_lines
 
 
 def day_07_part_1(my_input: InputType, debug: bool = False) -> int:
+    if debug:
+        pp(my_input)
     return -1    
 
 
@@ -22,11 +81,14 @@ def main():
     my_input = build_input_type(input_list)
 
     test_input = build_input_type("""\
-line 1
-line 2""".split('\n'))
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""".split('\n'))
 
     print(day_07_part_1(test_input, debug=True))
-    print(day_07_part_1(my_input))
+    print(day_07_part_1(my_input, debug=True))
 
     print(day_07_part_2(test_input, debug=True))
     print(day_07_part_2(my_input))
