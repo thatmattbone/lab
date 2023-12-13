994de4e1ea317113b6779f70c595babb0844a135
Matt Bone <thatmattbone@gmail.com>
2023-12-12 23:16:39 -0600

    day 08 skeleton

---
+++ advent/2023/advent/day_08/test_prob.py
@@ -0,0 +1,12 @@
+from advent.util import input_lines
+from .prob import day_08_part_1, day_08_part_2, build_input_type
+
+
+def test_day_08_part_1():
+    my_input = build_input_type(input_lines(__file__))
+    assert day_08_part_1(my_input) == -1
+
+
+def test_day_08_part_2():
+    my_input = build_input_type(input_lines(__file__))
+    assert day_08_part_2(my_input) == -1
