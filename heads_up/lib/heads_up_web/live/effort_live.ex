1a2a252eb7d0b7b5ff35689550d5c905c75952a2
Matt Bone <thatmattbone@gmail.com>
2025-03-09 21:07:02 -0500

    button on this liveview

--- heads_up/lib/heads_up_web/live/effort_live.ex
+++ heads_up/lib/heads_up_web/live/effort_live.ex
@@ -2,7 +2,7 @@ defmodule HeadsUpWeb.EffortLive do
   use HeadsUpWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, num_of_responders: 2, avg_minutes: 5)}
+    {:ok, assign(socket, num_of_responders: 1, avg_minutes: 5)}
   end
 
   def render(assigns) do
@@ -10,6 +10,9 @@ defmodule HeadsUpWeb.EffortLive do
       <div class="effort">
         <h1>Community Love</h1>
         <section>
+          <button phx-click="add">
+            + 1
+          </button>
           <div>
             <%= @num_of_responders %>
           </div>
@@ -24,5 +27,13 @@ defmodule HeadsUpWeb.EffortLive do
         </section>
       </div>
     """
+  end
+
+  def handle_event("add", _, socket) do
+    #new_num_of_responders = socket.assigns.num_of_responders + 1
+    #socket = assign(socket, num_of_responders: new_num_of_responders)
+    #socket = update(socket, :num_of_responders, fn num_of_responders -> num_of_responders + 1 end)
+    socket = update(socket, :num_of_responders, &(&1 + 1))
+    {:noreply, socket}
   end
 end
