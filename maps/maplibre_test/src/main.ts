e7343cd2121b52772701701240dabf7f7e260b5f
Matt Bone <thatmattbone@gmail.com>
2025-08-17 10:44:09 -0500

    create a marker w/popup

--- maps/maplibre_test/src/main.ts
+++ maps/maplibre_test/src/main.ts
@@ -1,9 +1,10 @@
-import maplibregl, {type LngLatLike} from 'maplibre-gl';
+import maplibregl, {type LngLatLike, Map} from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 // import './style.css'
 
 const INITIAL_ZOOM = 5;
 const ZOCALO: LngLatLike = [-99.1357763, 19.4326207];
+
 
 function build_map() {
     console.log("Hello from build_map in main.ts.");
@@ -16,4 +17,34 @@ function build_map() {
     });
 }
 
-build_map();
+
+function add_marker(map: Map) {
+    const paradero_conocido: LngLatLike = [-99.1476275, 19.431313];
+
+    const popup = new maplibregl.Popup({offset: 25}).setText(
+        'Zócalo is the common name of the main square in central Mexico City. ' +
+        'Prior to the colonial period, it was the main ceremonial center in the Aztec city of Tenochtitlan.'
+    );
+
+    //const image = await map.loadImage('https://maplibre.org/maplibre-gl-js/docs/assets/custom_marker.png');
+    //map.addImage('custom-marker', image.data);
+
+    // create DOM element for the marker
+    // const el = document.createElement('div');
+    // el.id = 'marker';
+
+    // create the marker
+    // new maplibregl.Marker({element: el})
+    //     .setLngLat(paradero_conocido)
+    //     .setPopup(popup) // sets a popup on this marker
+    //     .addTo(map);
+
+    let marker = new maplibregl.Marker({color: "#C11007"});
+    marker.setLngLat(paradero_conocido);
+    marker.setPopup(popup);
+    marker.addTo(map);
+}
+
+
+let map = build_map();
+add_marker(map);
